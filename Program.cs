fe7e6041d0f3c3c5f86c2bdceddac4d976021b48
vdallabona <vinicius.dallabona@gmail.com>
2024-04-27 11:03:19 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace introCSharp{
+    public class Program{
+
+        static void Main(string[] args){
+            int op;
+            float resultado = 0;
+            do{
+                Console.WriteLine("Digite qual operação deseja realizar.");
+                Console.WriteLine("\n [1] Soma");
+                Console.WriteLine("\n [2] Subtração");
+                Console.WriteLine("\n [3] Multiplicação");
+                Console.WriteLine("\n [4] Divisão");
+                Console.WriteLine("\n [5] Zerar valor");
+                Console.WriteLine("\n [7] Encerrar");
+                op = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n Digite o valor da operação: ");
+                float num1 = Convert.ToInt32(Console.ReadLine());
+                switch (op){
+                    case 1:{
+                        Console.Write($"\n {resultado} + {num1} = ");
+                        resultado = Operacoes.Somar(num1, resultado);
+                        Console.Write($"{resultado} \n");
+                        break;
+                    }
+                    case 2:{
+                        Console.Write($"\n {resultado} - {num1} = ");
+                        resultado = Operacoes.Subtrair(num1, resultado);
+                        Console.Write($"{resultado} \n");
+                        break;
+                    }
+                    case 3:{
+                        Console.Write($"\n {resultado} X {num1} = ");
+                        resultado = Operacoes.Multiplicar(num1, resultado);
+                        Console.Write($"{resultado} \n");
+                        break;
+                    }
+                    case 4:{
+                        Console.Write($"\n {resultado} / {num1} = ");
+                        resultado = Operacoes.Dividir(num1, resultado);
+                        Console.Write($"{resultado} \n");
+                        break;
+                    }
+                    case 5:{
+                        resultado = 0;
+                        Console.WriteLine($"Valor zerado");
+                        break;
+                    }
+                }
+            }while(op != 7);
+        }
+    }
+}
---
+++ operacoes.cs
@@ -0,0 +1,28 @@
+namespace introCSharp{
+    public class Operacoes{
+        public static float Somar(float num1, float num2){
+            
+            float resultadoSoma;
+            resultadoSoma = num2 + num1;
+            return resultadoSoma;
+        }
+        public static float Subtrair(float num1, float num2){
+            
+            float resultadoSubtracao;
+            resultadoSubtracao = num2 - num1;
+            return resultadoSubtracao;
+        }
+        public static float Multiplicar(float num1, float num2){
+            
+            float resultadoMultiplicacao;
+            resultadoMultiplicacao = num2 * num1;
+            return resultadoMultiplicacao;
+        }
+        public static float Dividir(float num1, float num2){
+            
+            float resultadoDivisao;
+            resultadoDivisao = num2 / num1;
+            return resultadoDivisao;
+        }
+    }
+}
